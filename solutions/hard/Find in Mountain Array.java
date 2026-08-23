93e3273673ae1e5f0af21b0820b4647bd0ad5b81
Tanuj <tanujthakur634@gmail.com>
2026-08-23 21:48:51 +0530

    Implement findInMountainArray method for mountain array
    Implemented the findInMountainArray method to locate a target in a mountain array using binary search in both ascending and descending parts. Added a helper method to find the peak of the mountain array.

--- solutions/hard/Find in Mountain Array.java
+++ solutions/hard/Find in Mountain Array.java
@@ -1,10 +1,40 @@
-// Title: Find in Mountain Array
-            // Difficulty: Hard
-            // Language: Java
-            // Link: https://leetcode.com/problems/find-in-mountain-array/
+/**
+ * // This is MountainArray's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * interface MountainArray {
+ *     public int get(int index);
+ *     public int length();
+ * }
+ */
 
-            else e=mid;
-        }
-        return s;
-    }
+class Solution {
+    public int findInMountainArray(int target, MountainArray mountainArr) {
+        int peak=findPeak(mountainArr);
+        int s=0,e=peak;
+        //ASCENDING CHECK FIRST TO FIND MIN INDEX 
+        while(s<=e){
+            int mid=s+(e-s)/2;
+            if(mountainArr.get(mid)==target) return mid;
+            if(mountainArr.get(mid)<target) s=mid+1;
+            else e=mid-1;
+        }
+        //DESCENDING CHECK AT LAST IF THE ELEMENT IS ON RIGHT ARR OR NOT
+        s=peak+1;e=mountainArr.length()-1;
+        while(s<=e){
+            int mid=s+(e-s)/2;
+            if(mountainArr.get(mid)==target) return mid;
+            if(mountainArr.get(mid)>target) s=mid+1;
+            else e=mid-1;
+        }
+        return -1;
+    }
+    static int findPeak(MountainArray mountainArr){
+        int s=0,e=mountainArr.length()-1;
+        while(s<e){
+            int mid=s+(e-s)/2;
+            if(mountainArr.get(mid)<mountainArr.get(mid+1)) s=mid+1;
+            else e=mid;
+        }
+        return s;
+    }
 }
