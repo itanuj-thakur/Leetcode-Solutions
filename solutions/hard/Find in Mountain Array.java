1856deb23988a91b2708ba9e8563c0dbdc01849e
Tanuj <tanujthakur634@gmail.com>
2026-08-23 21:44:55 +0530

    Add solution for Find in Mountain Array

---
+++ solutions/hard/Find in Mountain Array.java
@@ -0,0 +1,10 @@
+// Title: Find in Mountain Array
+            // Difficulty: Hard
+            // Language: Java
+            // Link: https://leetcode.com/problems/find-in-mountain-array/
+
+            else e=mid;
+        }
+        return s;
+    }
+}
