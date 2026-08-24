268161f42b1d7dd04d9b80d3ab6d1259f7bece8c
Tanuj <tanujthakur634@gmail.com>
2026-08-24 07:53:23 +0530

    Add solution for Missing Number

---
+++ solutions/easy/Missing Number.java
@@ -0,0 +1,13 @@
+// Title: Missing Number
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/missing-number/
+
+                int temp = arr[i];
+            if (arr[i] < length && arr[i] != arr[correctIndex]) {
+            int correctIndex = arr[i];
+        int i = 0,length=arr.length;
+        while (i < length) {
+        //Cyclic sort best as range[0,N]
+    public int missingNumber(int[] arr) {
+class Solution {
