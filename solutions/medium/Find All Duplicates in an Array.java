37160ada87abbcf1e5a83b03bcbb9a6e120306ed
Tanuj <tanujthakur634@gmail.com>
2026-08-26 12:06:17 +0530

    Add solution for finding duplicates in an array
    Implemented a solution to find all duplicates in an array using index mapping.

--- solutions/medium/Find All Duplicates in an Array.java
+++ solutions/medium/Find All Duplicates in an Array.java
@@ -1,6 +1,22 @@
-// Title: Find All Duplicates in an Array
-            // Difficulty: Medium
-            // Language: Java
-            // Link: https://leetcode.com/problems/find-all-duplicates-in-an-array/
+import java.util.ArrayList;
+import java.util.List;
 
-i
+class Solution {
+    public List<Integer> findDuplicates(int[] arr) {
+        List<Integer> result = new ArrayList<>();
+        int i = 0;
+        while (i < arr.length) {
+            int corrIndx = arr[i] - 1;
+            if (arr[i] != arr[corrIndx]) {
+                int temp = arr[i];
+                arr[i] = arr[corrIndx];
+                arr[corrIndx] = temp;
+            }
+            else i++;
+        }
+        for(int j=0;j<arr.length;j++){
+            if(arr[j]!=j+1) result.add(arr[j]);
+        }
+        return result;
+    }
+}
