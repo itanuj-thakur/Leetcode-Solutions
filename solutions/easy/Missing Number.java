ea9fc10c9e4bd09e71f13c08c20719bab4a71df6
Tanuj <tanujthakur634@gmail.com>
2026-08-26 12:07:30 +0530

    Refactor missingNumber to use sum formula
    Refactored missingNumber method to use mathematical approach for finding the missing number.

--- solutions/easy/Missing Number.java
+++ solutions/easy/Missing Number.java
@@ -1,20 +1,33 @@
-// Title: Missing Number
-            // Difficulty: Easy
-            // Language: Java
-            // Link: https://leetcode.com/problems/missing-number/
+class Solution {
+    public int missingNumber(int[] arr) {
+        int i=0;
+        //first sort till n-1
+        while(i<arr.length){
+            int correctIndex=arr[i];//as we are also given 0
+            if(arr[i]<arr.length && arr[i]!=arr[correctIndex]){
+                int temp=arr[i];
+                arr[i]=arr[correctIndex];
+                arr[correctIndex]=temp;
+            }
+            else i++;
+        }
+        for(int j=0;j<arr.length;j++){
+            if(arr[j]!=j) return j;
+        }
+        return arr.length;
+    }
+}
 
-                arr[i]=arr[correctIndex];
-                arr[correctIndex]=temp;
-            }
-            else i++;
-        }
-        for(int j=0;j<arr.length;j++){
-            if(arr[j]!=j) return j;
-                int temp=arr[i];
-            if(arr[i]<arr.length && arr[i]!=arr[correctIndex]){
-            int correctIndex=arr[i];//as we are also given 0
-        while(i<arr.length){
-        //first sort till n-1
-        int i=0;
-    public int missingNumber(int[] arr) {
-class Solution {
+//BY MATHS
+class Solution {
+    public int missingNumber(int[] arr) {
+        //Math approach total sum =n(n+1)/2
+        int len=arr.length;
+        int sum=len*(len+1)/2;
+        int actualSum=0;
+        for(int num : arr)
+        actualSum+=num;
+        return sum-actualSum;
+        
+    }
+}
