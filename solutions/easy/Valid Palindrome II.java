c3325501298672a9701c33ad9352791c3047ba0b
Tanuj <tanujthakur634@gmail.com>
2026-08-27 22:21:52 +0530

    Add solution for Valid Palindrome II

--- solutions/easy/Valid Palindrome II.java
+++ solutions/easy/Valid Palindrome II.java
@@ -3,14 +3,13 @@
             // Language: Java
             // Link: https://leetcode.com/problems/valid-palindrome-ii/
 
-    // Standard palindrome helper (0 deletions allowed)
-    private boolean isPalindrome(String s, int left, int right) {
-        while (left < right) {
-            if (s.charAt(left) != s.charAt(right)) {
-                return false;
-            }
-            left++;
-            right--;
-        }
         return true;
-    }
+        }
+            i++;j--;
+            }
+                return checkPalin(s,i+1,j) || checkPalin(s,i,j-1);
+            if(s.charAt(i)!=s.charAt(j)){
+        while(i<j){
+        int i=0,j=s.length()-1;
+    public boolean validPalindrome(String s) {
+class Solution {
