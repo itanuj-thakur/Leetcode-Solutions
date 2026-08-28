03cb0d29d3c0e3805e27a21bf8c22de0ab6ebca9
Tanuj <tanujthakur634@gmail.com>
2026-08-28 12:09:02 +0530

    Add solution for Pow(x, n)

---
+++ solutions/medium/Pow(x, n).java
@@ -0,0 +1,15 @@
+// Title: Pow(x, n)
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/pow(x,-n)/
+
+
+    }
+        return power(x, N);
+        }
+            return 1.0 / power(x, -N);
+        if (N < 0) {
+        long N = n;
+        // Handle edge case for integer overflow when n = Integer.MIN_VALUE
+    public double myPow(double x, int n) {
+class Solution {
