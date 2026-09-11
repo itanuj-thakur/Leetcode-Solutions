120191a31649b38b78be7ac667609b1ada85571e
Tanuj <tanujthakur634@gmail.com>
2026-09-11 12:50:37 +0530

    Add solution for Happy Number

---
+++ solutions/easy/Happy Number.java
@@ -0,0 +1,15 @@
+// Title: Happy Number
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/happy-number/
+
+            int s = 0;
+        while (true) {
+
+    public boolean isHappy(int n) {
+        Set<Integer> seen = new HashSet<>();
+class Solution {
+
+            while (n != 0) {
+                int d = n % 10;
+                s += d * d;
