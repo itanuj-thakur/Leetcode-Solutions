2e4939c5d3e553af72599c09fa6ce173609fa6d2
Tanuj <tanujthakur634@gmail.com>
2026-09-12 11:22:04 +0530

    Add solution for Fizz Buzz

---
+++ solutions/easy/Fizz Buzz.java
@@ -0,0 +1,10 @@
+// Title: Fizz Buzz
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/fizz-buzz/
+
+            else list.add(Integer.toString(i));
+        }
+        return list;
+    }
+}
